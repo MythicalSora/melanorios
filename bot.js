8b76ec8fd02a29660dc9023e5deff7fd2e82d491
Alwin <a.meijboom@outlook.com>
2018-04-19 11:19:54 +0200

    Tried to fix MySQL error

--- bot.js
+++ bot.js
@@ -4,7 +4,13 @@ const mysql = require('mysql');
 const process = require('process');
 const config = require('./config.json');
 
-let conn = mysql.createConnection(config.mysql);
+let conn = mysql.createConnection({
+  host : config.mysql.host,
+  user : config.mysql.user,
+  password : config.mysql.password,
+  database : config.mysql.database,
+  insecureAuth : true
+});
 
 conn.connect();
 
